9b4e168b7485f8f309c3666cc176dcd04d6ab043
Richie Viteri <richie.viteri@100starlings.com>
2021-04-19 20:59:17 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="">
+        
+      </header>
+      <body>
+        Hola mundo!
+      </body>
+    </div>
+  );
+}
+
+export default App;
