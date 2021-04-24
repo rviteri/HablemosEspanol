48905453a8cdf022b1a0493192f8c82713ebb85e
Richie Viteri <richie.viteri@100starlings.com>
2021-04-24 12:21:33 +0100

    Created hero banner component and implemented home hero banner.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './../components/Header';
 import Footer from './../components/Footer';
+import HeroBanner from './../components/HeroBanner';
 import { useTranslation } from 'react-i18next';
 
 function Home () {
@@ -8,10 +9,18 @@ function Home () {
 
     return (
         <>
-            <Header whiteStyle={false} transparent={true}></Header>
-            <div className="main-page-hero-banner">
-              {t('hablemos_espanol')}
-            </div>
+            <Header whiteStyle={true} transparent={true}></Header>
+            <HeroBanner
+                title={t("home_hero_title")}
+                subtitle={t("home_hero_subtitle")}
+                buttonText={t("home_hero_button")}
+                buttonUrl="/cursos"
+                verticalAlignment="bottom"
+                horizontalAlignment="left"
+                hasBigTitle={false}
+                withBottomMargin={false}
+                imageUrl="/images/bg/background_home.jpeg"
+                backgroundYPosition="90%"></HeroBanner>
             <Footer></Footer>
         </>
     );
--- src/resources/locales/en.js
+++ src/resources/locales/en.js
@@ -3,6 +3,9 @@ export const en = {
     "hablemos_espanol": "Hablemos Español",
     "cursos": "Cursos",
     "contactame": "Contáctame",
-    "inscripcion": "Inscripción"
+    "inscripcion": "Inscripción",
+    "home_hero_title": "Cursos de conversación en español en línea",
+    "home_hero_subtitle": "El siguiente curso comienza el 11 de Mayo de 2021",
+    "home_hero_button": "Más Información",
   }
 }
