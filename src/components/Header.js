b30b2d3f9e7e3c001b2a661f7a430af9dab0e359
Richie Viteri <richie.viteri@100starlings.com>
2021-04-24 12:20:56 +0100

    Improve header and footer styling and structure.

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,67 +28,70 @@ function Header ({whiteStyle = false, transparent = true}) {
 
     return (
         <>
-            <div className={`navbar${menuOpen ? " -menu-open" : ""}${whiteStyle && !headerTransparentStyle ? " -white" : ""}${!whiteStyle && !headerTransparentStyle ? " -dark" : ""}`}>
-                <div className="logo-container">
-                    <Link to="/" className="navbar-logo">
-                        {t("hablemos_espanol")}
-                    </Link>
-                </div>
-                <div className="menu-container">
-                    <Link to="/cursos" className="navbar-link">
-                        {t("cursos")}
-                    </Link>
-                    <Link to="/contactame" className="navbar-link">
-                        {t("contactame")}
-                    </Link>
-                    <Link to="/inscripcion" className="regular-button -short">
-                        {t("inscripcion")}
-                    </Link>
-                </div>
-                {!menuOpen ?
-                    <div className="mobile-menu-container" onClick={toggleMenu}>
-                        <MenuOutline
-                            color={whiteStyle ? '#000000' : '#FFFFFF'} 
-                            title={"menu"}
-                            height="40px"
-                            width="40px"/>
+            <section className={`section-header${menuOpen ? " -menu-open" : ""}${whiteStyle && !headerTransparentStyle ? " -white" : ""}${!whiteStyle && !headerTransparentStyle ? " -dark" : ""}`}>
+                <div className={`section-content navbar${menuOpen ? " -menu-open" : ""}${whiteStyle && !headerTransparentStyle ? " -white" : ""}${!whiteStyle && !headerTransparentStyle ? " -dark" : ""}`}>
+                    <div className="logo-container">
+                        <Link to="/" className="navbar-logo">
+                            {t("hablemos_espanol")}
+                        </Link>
                     </div>
-                :
-                    <div className="mobile-menu-container" onClick={toggleMenu}>
-                        <CloseOutline
-                            color={'#000000'} 
-                            title={"close"}
-                            height="40px"
-                            width="40px"/>
+                    <div className="menu-container">
+                        <Link to="/cursos" className="navbar-link">
+                            {t("cursos")}
+                        </Link>
+                        <Link to="/contactame" className="navbar-link">
+                            {t("contactame")}
+                        </Link>
+                        <Link to="/inscripcion" className="regular-button -short">
+                            {t("inscripcion")}
+                        </Link>
                     </div>
+                    {!menuOpen ?
+                        <div className="mobile-menu-container" onClick={toggleMenu}>
+                            <MenuOutline
+                                color={whiteStyle ? '#000000' : '#FFFFFF'} 
+                                title={"menu"}
+                                height="40px"
+                                width="40px"/>
+                        </div>
+                    :
+                        <div className="mobile-menu-container" onClick={toggleMenu}>
+                            <CloseOutline
+                                color={'#000000'} 
+                                title={"close"}
+                                height="40px"
+                                width="40px"/>
+                        </div>
 
-                }
+                    }
 
-                <div className="nav-mobile-menu">
-                    <ul className="nav-menu-list">
-                        <li className="nav-menu-list-item">
-                            <Link to="/cursos" className="nav-links" onClick={closeMobileMenu}>
-                                {t("cursos")}
-                            </Link>
-                        </li>
-                        <li className="nav-menu-list-item">
-                            <Link to="/contactame" className="nav-links" onClick={closeMobileMenu}>
-                                {t("contactame")}
-                            </Link>
-                        </li>
-                        <li className="nav-menu-list-item">
-                            <Link to="/contactame" className="nav-links" onClick={closeMobileMenu}>
-                                {t("contactame")}
-                            </Link>
-                        </li>
-                        <li className="nav-menu-list-item">
-                            <Link to="/inscripcion" className="nav-links" onClick={closeMobileMenu}>
-                                {t("inscripcion")}
-                            </Link>
-                        </li>
-                    </ul>
+                    <div className="nav-mobile-menu">
+                        <ul className="nav-menu-list">
+                            <li className="nav-menu-list-item">
+                                <Link to="/cursos" className="nav-links" onClick={closeMobileMenu}>
+                                    {t("cursos")}
+                                </Link>
+                            </li>
+                            <li className="nav-menu-list-item">
+                                <Link to="/contactame" className="nav-links" onClick={closeMobileMenu}>
+                                    {t("contactame")}
+                                </Link>
+                            </li>
+                            <li className="nav-menu-list-item">
+                                <Link to="/contactame" className="nav-links" onClick={closeMobileMenu}>
+                                    {t("contactame")}
+                                </Link>
+                            </li>
+                            <li className="nav-menu-list-item">
+                                <Link to="/inscripcion" className="nav-links" onClick={closeMobileMenu}>
+                                    {t("inscripcion")}
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
+            </section>
+            
         </>
     );
 }
